40e8330f605115cd5239d03f79726309a7ac6ae6
DennisArgemiro <dennisarg2011@gmail.com>
2023-09-17 16:00:26 -0300

    supabase config

---
+++ supabase/config.js
@@ -0,0 +1,6 @@
+module.exports ={
+    url: "https://ouwsmhqduwwmvmrylkmi.supabase.co", public:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91d3NtaHFkdXd3bXZtcnlsa21pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyNzk2NDgsImV4cCI6MjAwMTg1NTY0OH0.MPmSriQ49eXyV16O2GF7z5y6ipffINOwlZR7UF7-AjY",
+    private: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91d3NtaHFkdXd3bXZtcnlsa21pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjI3OTY0OCwiZXhwIjoyMDAxODU1NjQ4fQ.nL7iWkSs2vYU7oXw0hHPvM9280tUdt456obcnFqqTUY",
+    mainMail: "dennisarg2011@gmail.com",
+    mainPass: "Cafe.dennis2011"
+  }
