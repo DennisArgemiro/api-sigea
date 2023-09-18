11c8095fb2c0516f98a047e78e204f4e9b914b9c
DennisArgemiro <dennisarg2011@gmail.com>
2023-09-17 23:59:46 -0300

    redirecionando

--- api/redirect.js
+++ api/redirect.js
@@ -6,6 +6,6 @@ module.exports = async (req, res)=>{
     const solicitacao = await supabase.selectReclamacao("id", idSolicitacao)
     const setor = await supabase.selectSetor(idSetor)
     const aluno = await supabase.selectAluno(solicitacao.Aluno_matricula)
-
+    console.log(setor)
     res.json(await mail.redirectToSector(aluno.nome, solicitacao, setor.idSetor, setor.email)) 
 }
