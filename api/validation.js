3d1c9a8a7f0624a6942b75f5ee5c7cf5acaec3b3
DennisArgemiro <dennisarg2011@gmail.com>
2023-09-18 10:47:55 -0300

    redirecionando

--- api/validation.js
+++ api/validation.js
@@ -2,6 +2,7 @@ const supabase = require("../supabase/supabase")
 const path = require('path');
 
 module.exports = async (req, res) => {
+    console.log(req.query.values)
     const response = await supabase.insertAluno({values: req.query.values})
     response != 200 ? { status: 400 } : { status: 200 }
     res.json(response)
