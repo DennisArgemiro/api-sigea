0f01305101cb8c72935e2620bbda4ac09308bac6
DennisArgemiro <dennisarg2011@gmail.com>
2023-09-22 22:31:47 -0300

    update historico

--- api/updateSolicitacao.js
+++ api/updateSolicitacao.js
@@ -4,7 +4,8 @@ module.exports = async (req, res)=>{
     const { idSolicitacao, date, att } = req.query;
 
     const hist = await supabase.selectReclamacao("id", idSolicitacao)
-    console.log({date, att})
+    console.log(JSON.parse({date, att}))
+    
 
     hist.historico != undefined ? hist.historico[Object.keys(hist.historico).length] = JSON.parse({date, att}): hist.historico[0]= JSON.parse({date, att}) 
 
