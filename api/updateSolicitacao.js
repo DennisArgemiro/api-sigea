9b8bbffda492e056137c035e361afd8ef30ad58e
DennisArgemiro <dennisarg2011@gmail.com>
2023-09-22 22:25:50 -0300

    update historico

--- api/updateSolicitacao.js
+++ api/updateSolicitacao.js
@@ -1,12 +1,12 @@
 const supabase = require("../supabase/supabase")
 
 module.exports = async (req, res)=>{
-    const { idSolicitacao, object } = req.query;
+    const { idSolicitacao, date, att } = req.query;
 
     const hist = await supabase.selectReclamacao("id", idSolicitacao)
     console.log(hist.historico)
 
-    hist.historico != undefined ? hist.historico[Object.keys(hist.historico).length] = JSON.parse(object): hist[0]= JSON.parse(object) 
+    hist.historico != undefined ? hist.historico[Object.keys(hist.historico).length] = JSON.parse({date, att}): hist[0]= JSON.parse({date, att}) 
 
     const data = await supabase.updateHistorico(idSolicitacao, hist.historico)
 
