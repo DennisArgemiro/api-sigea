ab7af37894beb041654dbaea6da7e7608ef926cc
Dennis Argemiro <88357881+DennisArgemiro@users.noreply.github.com>
2023-10-07 19:54:25 -0300

    att foda

--- api/selectAluno.js
+++ api/selectAluno.js
@@ -1,7 +1,7 @@
 const supabase = require("../supabase/supabase")
 
 module.exports =  async (req, res) => {
-    const { param, value } = req.query
+    const { param, value} = req.query
     const response = await supabase.selectAluno(param, value)
     res.json(response)
   }
